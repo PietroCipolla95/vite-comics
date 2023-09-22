a2b09ac2ca0e0a9d4319f9412cb1c0aed02a2139
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-09-22 19:55:31 +0200

    add footer tag

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,148 +5,150 @@ export default {
 </script>
 
 <template>
-    <!-- main footer -->
-    <section class="main_footer">
-        <div class="row my_container flex-nowrap">
-            <!-- columns on left with all links -->
-            <div class="col-5 py-4">
-                <div class="row py-4">
-                    <!-- dc comics and shop col wrapper -->
-                    <div class="col-4">
-                        <!-- DC comics section -->
-                        <div class="col-12">
-                            <h6>
-                                DC COMICS
-                            </h6>
-                            <div class="d-flex flex-column">
-                                <a href="#">
-                                    Characters
-                                </a>
-                                <a href="#">
-                                    Comics
-                                </a>
-                                <a href="#">
-                                    Movies
-                                </a>
-                                <a href="#">
-                                    TV
-                                </a>
-                                <a href="#">
-                                    Games
-                                </a>
-                                <a href="#">
-                                    Videos
-                                </a>
-                                <a href="#">
-                                    Games
-                                </a>
+    <footer>
+        <!-- main footer -->
+        <section class="main_footer">
+            <div class="row my_container flex-nowrap">
+                <!-- columns on left with all links -->
+                <div class="col-5 py-4">
+                    <div class="row py-4">
+                        <!-- dc comics and shop col wrapper -->
+                        <div class="col-4">
+                            <!-- DC comics section -->
+                            <div class="col-12">
+                                <h6>
+                                    DC COMICS
+                                </h6>
+                                <div class="d-flex flex-column">
+                                    <a href="#">
+                                        Characters
+                                    </a>
+                                    <a href="#">
+                                        Comics
+                                    </a>
+                                    <a href="#">
+                                        Movies
+                                    </a>
+                                    <a href="#">
+                                        TV
+                                    </a>
+                                    <a href="#">
+                                        Games
+                                    </a>
+                                    <a href="#">
+                                        Videos
+                                    </a>
+                                    <a href="#">
+                                        Games
+                                    </a>
+                                </div>
+                            </div>
+                            <!-- Shop section -->
+                            <div class="col-12 pt-4">
+                                <h6>
+                                    SHOP
+                                </h6>
+                                <div class="d-flex flex-column">
+                                    <a href="#">
+                                        Shop DC
+                                    </a>
+                                    <a href="#">
+                                        Shop DC Collectibles
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                        <!-- Shop section -->
-                        <div class="col-12 pt-4">
+                        <!-- Dc section -->
+                        <div class="col-4">
                             <h6>
-                                SHOP
-                            </h6>
-                            <div class="d-flex flex-column">
-                                <a href="#">
-                                    Shop DC
-                                </a>
-                                <a href="#">
-                                    Shop DC Collectibles
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Dc section -->
-                    <div class="col-4">
-                        <h6>
-                            DC
-                        </h6>
-                        <div class="d-flex flex-column">
-                            <a href="#">
-                                Terms of use
-                            </a>
-                            <a href="#">
-                                Privacy policy (New)
-                            </a>
-                            <a href="#">
-                                Ad Choices
-                            </a>
-                            <a href="#">
-                                Advertising
-                            </a>
-                            <a href="#">
-                                Jobs
-                            </a>
-                            <a href="#">
-                                Subscriptions
-                            </a>
-                            <a href="#">
-                                Talent Workshop
-                            </a>
-                            <a href="#">
-                                CPSC Certificates
-                            </a>
-                            <a href="#">
-                                Ratings
-                            </a>
-                            <a href="#">
-                                Shop Help
-                            </a>
-                            <a href="#">
-                                Contact Us
-                            </a>
-                        </div>
-                    </div>
-                    <!-- Sites section -->
-                    <div class="col-4">
-                        <h6>
-                            SITES
-                        </h6>
-                        <div class="d-flex flex-column">
-                            <a href="#">
                                 DC
-                            </a>
-                            <a href="#">
-                                MAD Magazine
-                            </a>
-                            <a href="#">
-                                DC Kids
-                            </a>
-                            <a href="#">
-                                DC Universe
-                            </a>
-                            <a href="#">
-                                DC Power Vista
-                            </a>
+                            </h6>
+                            <div class="d-flex flex-column">
+                                <a href="#">
+                                    Terms of use
+                                </a>
+                                <a href="#">
+                                    Privacy policy (New)
+                                </a>
+                                <a href="#">
+                                    Ad Choices
+                                </a>
+                                <a href="#">
+                                    Advertising
+                                </a>
+                                <a href="#">
+                                    Jobs
+                                </a>
+                                <a href="#">
+                                    Subscriptions
+                                </a>
+                                <a href="#">
+                                    Talent Workshop
+                                </a>
+                                <a href="#">
+                                    CPSC Certificates
+                                </a>
+                                <a href="#">
+                                    Ratings
+                                </a>
+                                <a href="#">
+                                    Shop Help
+                                </a>
+                                <a href="#">
+                                    Contact Us
+                                </a>
+                            </div>
+                        </div>
+                        <!-- Sites section -->
+                        <div class="col-4">
+                            <h6>
+                                SITES
+                            </h6>
+                            <div class="d-flex flex-column">
+                                <a href="#">
+                                    DC
+                                </a>
+                                <a href="#">
+                                    MAD Magazine
+                                </a>
+                                <a href="#">
+                                    DC Kids
+                                </a>
+                                <a href="#">
+                                    DC Universe
+                                </a>
+                                <a href="#">
+                                    DC Power Vista
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class=".col-7 logo d-flex flex-shrink-1 align-self-center justify-content-center">
-            </div>
-        </div>
-    </section>
-    <!-- bottom footer -->
-    <section class="bottom_footer">
-        <div class="row my_container py-5 align-items-center justify-content-between">
-            <!-- col with sign up button -->
-            <div class="col-6">
-                <button type="button" class="btn my_border text-light P-2">SIGN UP NOW</button>
-            </div>
-            <!-- col with follow us and medias -->
-            <div class="col-4 d-flex align-items-center justify-content-between">
-                <p class="follow">FOLLOW US</p>
-                <!-- wrapper with medias -->
-                <div class="media_wrapper text-dark pb-3">
-                    <span>F</span>
-                    <span>F</span>
-                    <span>F</span>
-                    <span>F</span>
+                <div class=".col-7 logo d-flex flex-shrink-1 align-self-center justify-content-center">
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+        <!-- bottom footer -->
+        <section class="bottom_footer">
+            <div class="row my_container py-5 align-items-center justify-content-between">
+                <!-- col with sign up button -->
+                <div class="col-6">
+                    <button type="button" class="btn my_border text-light P-2">SIGN UP NOW</button>
+                </div>
+                <!-- col with follow us and medias -->
+                <div class="col-4 d-flex align-items-center justify-content-between">
+                    <p class="follow">FOLLOW US</p>
+                    <!-- wrapper with medias -->
+                    <div class="media_wrapper text-dark pb-3">
+                        <span>F</span>
+                        <span>F</span>
+                        <span>F</span>
+                        <span>F</span>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </footer>
 </template>
 
 
