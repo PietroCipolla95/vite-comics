78005f0332cf8f801af190225c448aa01f1d86e1
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-09-25 19:35:29 +0200

    add current series and load more button

---
+++ src/components/AppComicsCard.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+
+    name: 'AppComics',
+
+    props: {
+        series: String,
+        thumb: String,
+    }
+
+}
+</script>
+
+
+<template>
+    <div class="card">
+        <div class="comics_img">
+            <img :src="thumb">
+        </div>
+        <div class="comic_title">
+            <p>
+                {{ series }}
+            </p>
+        </div>
+
+    </div>
+</template>
+
+
+<style lang="scss" scoped></style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,14 +1,24 @@
 <script>
+import comics from '../comics.js';
+
 import AppJumbotron from './AppJumbotron.vue';
-import AppComics from './AppComics.vue';
+
+import AppComicsCard from './AppComicsCard.vue';
+
 
 
 export default {
     name: 'AppMain',
 
+    data() {
+        return {
+            comics
+        }
+    },
+
     components: {
         AppJumbotron,
-        AppComics
+        AppComicsCard
     }
 
 }
@@ -18,11 +28,57 @@ export default {
 
 <template>
     <main>
-        <AppJumbotron></AppJumbotron>
-        <AppComics></AppComics>
+        <AppJumbotron />
+
+        <div class="comics_sec">
+            <!-- cards row -->
+            <div class="row py-5 my_container">
+                <!-- current series hover -->
+                <div class="current_series position-relative">
+                    <div class="btn bg-dc my_btn_abs rounded-0 px-4 text-light">
+                        <h5 class="fs-4">
+                            CURRENT SERIES
+                        </h5>
+                    </div>
+                </div>
+
+                <div class="col-2" v-for="comic in comics">
+
+                    <AppComicsCard :thumb="comic.thumb" :series="comic.series"></AppComicsCard>
+
+                </div>
+            </div>
+
+            <div class="col-2 d-flex m-auto py-3">
+                <button class="btn bg-dc rounded-0 px-4 py-2 text-light fw-bolder">
+                    LOAD MORE
+                </button>
+            </div>
+
+        </div>
+
 
     </main>
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '../assets/scss/partials/variables.scss' as *;
+
+.comics_sec {
+    background-color: rgb(46, 42, 42);
+
+    .my_btn_abs {
+        position: absolute;
+        top: -4.5rem;
+        left: -1rem;
+    }
+
+    .bg-dc {
+        background-color: $dc-primary;
+    }
+
+
+
+}
+</style>
